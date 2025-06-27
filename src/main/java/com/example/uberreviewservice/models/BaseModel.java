d04bde5b74d5d90a33e1251f9813ea11e127f637
Ankit Jha <kumarjhaankit0@gmail.com>
2025-06-28 01:48:24 +0530

    feat: added setup

--- src/main/java/com/example/uberreviewservice/models/BaseModel.java
+++ src/main/java/com/example/uberreviewservice/models/BaseModel.java
@@ -2,7 +2,6 @@ package com.example.uberreviewservice.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
